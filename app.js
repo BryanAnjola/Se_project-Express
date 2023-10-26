bf1d58fb35c849dde0a85970dec93a55ba425f14
Bryananjola <heyitsbryxn@gmail.com>
2023-10-25 19:19:39 -0600

    ready for review

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
+require("dotenv").config();
 const express = require("express");
 const errorHandler = require("./middlewares/error-handler");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const { errors } = require("celebrate");
+const { limiter } = require("./utils/limiter");
+const { errorHandler } = require("./middlewares/error-handler");
+const { requestLogger, errorLogger } = require("./middlewares/logger");
 //app.js
 
 const { requestLogger, errorLogger } = require("./middlewares/logger");
@@ -13,18 +17,23 @@ mongoose.connect("mongodb://127.0.0.1:27017/wtwr_db");
 
 const routes = require("./routes");
 
+app.get("/crash-test", () => {
+  setTimeout(() => {
+    throw new Error("Server will crash now");
+  }, 0);
+});
+
 app.use(express.json());
 app.use(cors());
 app.use(requestLogger);
 app.use(routes);
 app.use(errorLogger);
 app.use(errors());
-// app.use(errorHandler);
+app.use(errorHandler);
 // //app.use((err, req, res, next) => {
 //   res.status(500).send({ message: 'An error occurred on the server' });
 // });
 app.listen(PORT, () => {
   console.log(`App listening at ${PORT}`);
-  console.log("this is still 
-working");
+  console.log("this is working");
 });
