91eb5ef1d5da648606e6a860aa286b5ea30a5bed
Bryananjola <heyitsbryxn@gmail.com>
2023-10-30 18:36:01 -0600

    deleted spaces in app.js

--- app.js
+++ app.js
@@ -7,14 +7,10 @@ const { errorHandler } = require("./middlewares/error-handler");
 const { requestLogger, errorLogger } = require("./middlewares/logger");
 const { PORT = 3001 } = process.env;
 const app = express();
-
 mongoose.connect("mongodb://127.0.0.1:27017/wtwr_db");
-
 const routes = require("./routes");
-
 app.use(express.json());
 app.use(cors());
-
 app.get("/crash-test", () => {
   setTimeout(() => {
     throw new Error("Server will crash now");
