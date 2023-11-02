3dd95529a50f1727679df2c07ada7be995c18a56
Bryananjola <heyitsbryxn@gmail.com>
2023-11-01 18:15:02 -0600

    fixing eslint errors

--- app.js
+++ app.js
@@ -9,9 +9,11 @@ const { requestLogger, errorLogger } = require("./middlewares/logger");
 const { PORT = 3001 } = process.env;
 
 const { limiter } = require("./utils/limiter");
+
 const app = express();
 
 mongoose.connect("mongodb://127.0.0.1:27017/wtwr_db");
+
 const routes = require("./routes");
 app.use(express.json());
 app.use(cors());
